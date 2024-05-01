263c29d7bfa90192132a7e59d4cf4927ce90070e
Abigail Ross-Whichello <arosswhich@gmail.com>
2024-04-30 20:47:52 -0400

    adding js for buttons

---
+++ assets/js/form.js
@@ -0,0 +1,14 @@
+function storeUsername(){
+let input=document.getElementById("userName").value;
+console.log(input)
+}
+
+function storeTitle(){
+    let input=document.getElementById("title").value;
+    console.log(input)
+}
+
+function storeContent(){
+    let input=document.getElementById("content").value;
+    console.log(input)
+}    
