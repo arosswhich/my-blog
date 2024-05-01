89039190244c543c5b52598292e156292382bdd4
Abigail Ross-Whichello <arosswhich@gmail.com>
2024-04-30 21:02:47 -0400

    nextPage function added

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,18 @@
 function storeUsername(){
-let input=document.getElementById("userName").value;
-console.log(input)
+let input1=document.getElementById("userName").value;
+console.log(input1)
 }
 
 function storeTitle(){
-    let input=document.getElementById("title").value;
-    console.log(input)
+    let input2=document.getElementById("title").value;
+    console.log(input2)
 }
 
 function storeContent(){
-    let input=document.getElementById("content").value;
-    console.log(input)
+    let input3=document.getElementById("content").value;
+    console.log(input3)
 }    
+
+function nextPage() {
+    window.location.href = "/blog.html";
+}
