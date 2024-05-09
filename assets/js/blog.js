f7362a035c9d33df15d0502d3951648a27170aec
Abigail Ross-Whichello <arosswhich@gmail.com>
2024-05-09 19:58:21 -0400

    working on fixing how local storage stores usernames

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,13 +1,2 @@
-const usernameStr2 = localStorage.getItem('username')
-const username2 = JSON.parse(usernameStr2)
-const titleStr2 = localStorage.getItem('title')
-const title2 = JSON.parse(titleStr2)
-const contentStr2 = localStorage.getItem('content')
-const content2 = JSON.parse(contentStr2)
-document.getElementById('name').innerHTML = username2;
-//document.getElementById('title').innerHTML = title2;
-//document.getElementById('content').innerHTML = content2;
-
-const postStr2 = localStorage.getItem('blogPost')
-const post2 = JSON.parse(postStr2)
-console.log(post2)
+let userName = JSON.parse(localStorage.getItem('user'));
+document.getElementById("header").innerHTML = "Hello, " + userName[5];
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,33 +1,26 @@
 
-//  function newArray(){
-    // let input1 = document.getElementById('username').value;
-    // let input2 = document.getElementById('title').value;
-    // let input3 = document.getElementById('content').value;
-    // let userArray = [];
-    // userArray.push(input1, input2,input3);
-    // console.log(userArray);
-    // let info = JSON.stringify(userArray);
-    // localStorage.setItem('info', info);
-    // let retInfo = localStorage.getItem('info');
-    // let retArray = JSON.parse(retInfo);
-    // console.log(retArray)
-// }
-
+var blogPosts = [];
 function storeInput() {
-   var input1 = document.getElementById('username').value;
-   var input2 = document.getElementById('title').value;
-   var input3 = document.getElementById('content').value; 
-   const blogPosts = [];
-   blogPosts.push(input1, input2, input3)
-   console.log(blogPosts)
-   localStorage.setItem('blogPost', JSON.stringify(blogPosts));
-   localStorage.setItem('username', JSON.stringify(input1))
-   localStorage.setItem('title', JSON.stringify(input2))
-   localStorage.setItem('content', JSON.stringify(input3))
-   if (input1 === '' || input2 === '' ||input3 === ''){
-    alert("Please enter your username, title, and content")
-   } else {
-    location.href = 'blog.html'
-   }
+const name = [];
+let usernames = JSON.parse(localStorage.getItem('user'));
+if (usernames === null) {
+     usernames = [];
+};
+const newUsername = document.getElementById('username').value;
+
+ if (usernames === null) {
+     name.push(newUsername);
+     localStorage.setItem("user",JSON.stringify(name));
+}
+else {
+     usernames.push(newUsername);
+     localStorage.setItem("user", JSON.stringify(usernames));   
+}
+
+if (newUsername !== '') {
+     window.location.assign('blog.html')
+} else {
+     alert("Please enter your Username to proceed")
+}
 
 }
