54c20fcc13f8b4296f9ae8c822ad63df35993b77
Abigail Ross-Whichello <arosswhich@gmail.com>
2024-05-09 23:50:27 -0400

    Functional stored arrays and slicing them to display

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,2 +1,44 @@
-let userName = JSON.parse(localStorage.getItem('user'));
-document.getElementById("header").innerHTML = "Hello, " + userName[5];
+//let userName = JSON.parse(localStorage.getItem('user'));
+//document.getElementById("header").innerHTML = "Hello, " + userName[0];
+//function displayPosts() {
+let blogPost = JSON.parse(localStorage.getItem('post'));
+console.log(blogPost)
+
+if (blogPost[4]){
+    console.log('4')
+    post5 = blogPost.slice(4,5)
+    post4 = blogPost.slice(3,4)
+    post3 = blogPost.slice(2,3)
+    post2 = blogPost.slice(1,2)
+    post1 = blogPost.slice(0,1)
+    console.log((document.getElementById("name5").innerHTML = 'Hello, ' + post5[0][0]))
+} else if (blogPost[3]){
+    console.log('3')
+    post4 = blogPost.slice(3,4)
+    post3 = blogPost.slice(2,3)
+    post2 = blogPost.slice(1,2)
+    post1 = blogPost.slice(0,1)
+    console.log((document.getElementById("name4").innerHTML = 'Hello, ' + post4[0][0]))
+} else if (blogPost[2]){
+    console.log('2')
+    post3 = blogPost.slice(2,3)
+    post2 = blogPost.slice(1,2)
+    post1 = blogPost.slice(0,1)
+    console.log((document.getElementById("name3").innerHTML = 'Hello, ' + post3[0][0]))
+} else if (blogPost[1]){
+    console.log('1')
+    post2 = blogPost.slice(1,2)
+    post1 = blogPost.slice(0,1)
+    console.log((document.getElementById("name2").innerHTML = 'Hello, ' + post2[0][0]))
+} 
+else {
+    console.log('0')
+    post1 = blogPost.slice(0,1)
+    console.log((document.getElementById("name1").innerHTML = 'Hello, ' + post1[0][0]))
+} 
+
+
+
+
+//}
+//displayPosts();
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,26 +1,55 @@
+/* function storeInput() {
+     const name = [];
+     let usernames = JSON.parse(localStorage.getItem('user'));
+     if (usernames === null) {
+          usernames = [];
+     };
+     if (usernames.length >= 10) {
+          usernames.length = 0;
+     }
+     const newUsername = document.getElementById('username').value;
+     
+      if (usernames === null) {
+          name.push(newUsername);
+          localStorage.setItem("user",JSON.stringify(name));
+     }
+     else {
+          usernames.push(newUsername);
+          localStorage.setItem("user", JSON.stringify(usernames));   
+     }
+     if (newUsername !== '') {
+          window.location.assign('blog.html')
+     } else {
+          alert("Please enter your Username to proceed")
+     }
+     
+     } */
 
-var blogPosts = [];
-function storeInput() {
-const name = [];
-let usernames = JSON.parse(localStorage.getItem('user'));
-if (usernames === null) {
-     usernames = [];
-};
-const newUsername = document.getElementById('username').value;
+     function storeInput() {
+          const blogPost = [];
+          let posts = JSON.parse(localStorage.getItem('post'));
+          if (posts === null) {
+               posts = [];
+          };
+          if (posts.length > 5) {
+               posts.length = 0;
+          }
+          var username = document.getElementById('username').value;
+          var title = document.getElementById('title').value;
+          var content = document.getElementById('content').value;
+          const newPost = [username, title, content]
+          if (posts === null) {
+               blogPost.push(newPost);
+               localStorage.setItem('post',JSON.stringify(blogPost));
+          }
+          else {
+               posts.push(newPost);
+               localStorage.setItem("post", JSON.stringify(posts));   
+          }
+          if (username !== '' && title !== '' && content !== '') {
+               window.location.assign('blog.html')
+          } else {
+               alert("Please enter your Username, Title, and Content to proceed")
+          }
 
- if (usernames === null) {
-     name.push(newUsername);
-     localStorage.setItem("user",JSON.stringify(name));
-}
-else {
-     usernames.push(newUsername);
-     localStorage.setItem("user", JSON.stringify(usernames));   
-}
-
-if (newUsername !== '') {
-     window.location.assign('blog.html')
-} else {
-     alert("Please enter your Username to proceed")
-}
-
-}
+     }
