2b8701cf31354daf60dba6e9fdec08427573890d
Abigail Ross-Whichello <arosswhich@gmail.com>
2024-05-11 23:04:00 -0400

    added some css

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,29 +1,3 @@
-/* function storeInput() {
-     const name = [];
-     let usernames = JSON.parse(localStorage.getItem('user'));
-     if (usernames === null) {
-          usernames = [];
-     };
-     if (usernames.length >= 10) {
-          usernames.length = 0;
-     }
-     const newUsername = document.getElementById('username').value;
-     
-      if (usernames === null) {
-          name.push(newUsername);
-          localStorage.setItem("user",JSON.stringify(name));
-     }
-     else {
-          usernames.push(newUsername);
-          localStorage.setItem("user", JSON.stringify(usernames));   
-     }
-     if (newUsername !== '') {
-          window.location.assign('blog.html')
-     } else {
-          alert("Please enter your Username to proceed")
-     }
-     
-     } */
 
      function storeInput() {
           const blogPost = [];
